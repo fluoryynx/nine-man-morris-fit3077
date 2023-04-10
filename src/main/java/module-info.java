1fb2fecc66d45902b51e98efa16082ecd796b66d
gsoo0005 <gsoo0005@monash.edu.my>
2023-04-10 14:59:33 +0800

    Added file structure for UI and logic and application class that open the interface

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module app.nmm {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens app.nmm to javafx.fxml;
+    exports app.nmm;
+}
