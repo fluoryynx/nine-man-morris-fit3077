dd18863508301310173fe0358737e46860c68134
jchu0057 <jchu0057@student.monash.edu>
2023-04-23 00:37:14 +0800

    Created new controller for board.fxml and had progress to link two files together

--- src/main/java/app/nmm/Controller/GameController.java
+++ src/main/java/app/nmm/Controller/GameController.java
@@ -1,4 +1,50 @@
 package app.nmm.Controller;
 
-public class GameController {
+import app.nmm.Application;
+import app.nmm.Data;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.util.Pair;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GameController implements Initializable {
+    Parent fxmlLoader;
+    Stage stage;
+    Scene scene;
+    Pair<Integer, String> winner;
+    Pair<Integer, String> loser;
+    @FXML Text p1;
+    @FXML Text p2;
+
+    @Override
+    @FXML
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("im in game scene");
+        System.out.println(scene);
+        Data data = (Data) stage.getUserData();
+        System.out.println(data);
+        winner = data.getWinner();
+        loser = data.getLoser();
+        p1.setText(winner.getValue());
+        p2.setText(loser.getValue());
+    }
+
+    void setP1(Pair<Integer, String> p1){
+        this.winner = p1;
+    }
+
+    void setP2(Pair<Integer, String> p2){
+        this.loser = p2;
+    }
 }
--- src/main/java/app/nmm/Controller/SelectTokenController.java
+++ src/main/java/app/nmm/Controller/SelectTokenController.java
@@ -1,33 +1,43 @@
 package app.nmm.Controller;
 
 import app.nmm.Application;
+import app.nmm.Data;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.util.Pair;
+
+import java.net.URL;
 import java.util.Random;
 
 import java.io.IOException;
+import java.util.ResourceBundle;
 
-public class SelectTokenController {
+public class SelectTokenController implements Initializable {
 
     Parent fxmlLoader;
     Stage stage;
     Scene scene;
     @FXML
-    Button getscene;
-
-
+    TextField player1Field;
     @FXML
-    public void initialize(){
+    TextField player2Field;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
         System.out.println("im in select token");
+        System.out.println(scene);
+        System.out.println(fxmlLoader);
     }
 
     @FXML
@@ -42,24 +52,42 @@ public class SelectTokenController {
 
     @FXML
     void flipCoin(MouseEvent event) throws IOException{
+        String str1 = player1Field.getText();
+        String str2 = player2Field.getText();
         Random r = new Random();
         int headOrTail = r.nextInt(100) + 1;
+        Pair<Integer, String> winner = null;
+        Pair<Integer, String> loser = null;
         if (headOrTail >= 50) {
-
+            winner = new Pair<>(1, "p1");
+            loser = new Pair<>(2, "p2");
         }
         else{
-
+            winner = new Pair<>(1, "p2");
+            loser = new Pair<>(2, "p1");
         }
+        System.out.println(fxmlLoader);
+        changeToGameScene(winner, loser, event);
     }
 
     @FXML
     void swap(MouseEvent event) throws IOException{
         // how to swap scene
-        Text textField1 = (Text) scene.lookup("p1");
-        Text textField2 = (Text) scene.lookup("p2");
-        String str1 = textField1.getText();
-        String str2 = textField2.getText();
-        textField1.setText(str2);
-        textField2.setText(str1);
+        String str1 = player1Field.getText();
+        String str2 = player2Field.getText();
+        player1Field.setText(str2);
+        player2Field.setText(str1);
+    }
+
+    void changeToGameScene(Pair<Integer, String> winner, Pair<Integer, String> loser, MouseEvent event) throws IOException {
+        Data data = new Data(winner, loser);
+        System.out.println(data);
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        fxmlLoader =  FXMLLoader.load(Application.class.getResource("board.fxml"));
+        stage.setUserData(data);
+        scene = new Scene(fxmlLoader);
+        stage.setScene(scene);
+        stage.setTitle("Nine Man's Morris");
+        stage.show();
     }
 }
