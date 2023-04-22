e0fb953dfa133d67e02f5c1f091331995e2fe4ee
jchu0057 <jchu0057@student.monash.edu>
2023-04-23 00:37:14 +0800

    Created new controller for board.fxml and had progress to link two files together

--- src/main/java/app/nmm/Application.java
+++ src/main/java/app/nmm/Application.java
@@ -1,11 +1,9 @@
 package app.nmm;
 
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -15,9 +13,8 @@ public class Application extends javafx.application.Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        Parent fxmlLoader = FXMLLoader.load(Application.class.getResource("main.fxml"));
-        Scene scene = new Scene(fxmlLoader);
-        stage.initStyle(StageStyle.UTILITY);
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("main.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Nine Man's Morris");
         stage.setScene(scene);
         stage.show();
