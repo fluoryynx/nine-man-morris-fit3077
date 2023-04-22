f7ca7f5410d1a3cb7837497fbc56f5f9d0036c9c
jchu0057 <jchu0057@student.monash.edu>
2023-04-22 21:25:32 +0800

    Created new controller clases and added new incomplete functionality

--- src/main/java/app/nmm/Controller/MainPageController.java
+++ src/main/java/app/nmm/Controller/MainPageController.java
@@ -1,29 +1,27 @@
 package app.nmm.Controller;
 
 import app.nmm.Application;
-import app.nmm.Data;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-import javafx.util.Pair;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class MainPageController implements Initializable {
+public class MainPageController {
 
-    @FXML AnchorPane mainPageScene;
+    Parent fxmlLoader;
+    Stage stage;
+    Scene scene;
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
+    @FXML
+    public void initialize(){
         System.out.println("im in main page");
     }
 
@@ -34,39 +32,12 @@ public class MainPageController implements Initializable {
 
     @FXML
     void playerVsPlayerMode(MouseEvent event)throws IOException {
-        AnchorPane tokenScene = FXMLLoader.load(Application.class.getResource("SelectToken.fxml"));
-        System.out.println(mainPageScene);
-        mainPageScene.getChildren().removeAll();
-        mainPageScene.getChildren().setAll(tokenScene);
-    }
-
-    @FXML
-    void playerVsComputerMode(MouseEvent event)throws IOException {
-        // preset the winner loser data
-        // winner is always Player 1
-        // loser is always Player 2
-        Pair<Integer, String> winner = new Pair<>(1, "p1");
-        Pair<Integer, String> loser = new Pair<>(2, "computer");
-        Data data = new Data (winner, loser, "computer");
-
-        AnchorPane boardScene = FXMLLoader.load(Application.class.getResource("board.fxml"));
-        System.out.println(mainPageScene);
-        mainPageScene.getChildren().removeAll();
-        mainPageScene.getChildren().setAll(boardScene);
-    }
-
-    @FXML
-    void tutorialMode(MouseEvent event)throws IOException {
-        // preset the winner loser data
-        // winner or loser does not matter in this case
-        Pair<Integer, String> winner = new Pair<>(1, "p1");
-        Pair<Integer, String> loser = new Pair<>(2, "tutorial");
-        Data data = new Data (winner, loser, "tutorial");
-
-        AnchorPane boardScene = FXMLLoader.load(Application.class.getResource("board.fxml"));
-        System.out.println(mainPageScene);
-        mainPageScene.getChildren().removeAll();
-        mainPageScene.getChildren().setAll(boardScene);
+        fxmlLoader = FXMLLoader.load(Application.class.getResource("SelectToken.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(fxmlLoader);
+        System.out.println(scene);
+        stage.setTitle("Nine Man's Morris");
+        stage.show();
     }
 
 
--- src/main/java/app/nmm/Controller/SelectTokenController.java
+++ src/main/java/app/nmm/Controller/SelectTokenController.java
@@ -1,98 +1,65 @@
 package app.nmm.Controller;
 
 import app.nmm.Application;
-import app.nmm.Data;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.FlowPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import javafx.util.Pair;
-
-import java.net.URL;
 import java.util.Random;
 
 import java.io.IOException;
-import java.util.ResourceBundle;
 
-public class SelectTokenController implements Initializable {
+public class SelectTokenController {
+
+    Parent fxmlLoader;
+    Stage stage;
+    Scene scene;
+    @FXML
+    Button getscene;
+
 
     @FXML
-    Text player1Field;
-    @FXML
-    Text player2Field;
-    @FXML
-    AnchorPane tokenScene;
-
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
+    public void initialize(){
         System.out.println("im in select token");
     }
 
     @FXML
     void backToMain(MouseEvent event) throws IOException {
-        AnchorPane mainPageScene = FXMLLoader.load(Application.class.getResource("main.fxml"));
-        tokenScene.getChildren().removeAll();
-        tokenScene.getChildren().setAll(mainPageScene);
+        fxmlLoader = FXMLLoader.load(Application.class.getResource("main.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(fxmlLoader);
+        stage.setScene(scene);
+        stage.setTitle("Nine Man's Morris");
+        stage.show();
     }
 
     @FXML
     void flipCoin(MouseEvent event) throws IOException{
-        // flip coin method, got a bit of bug
-        String str1 = player1Field.getText();
-        String str2 = player2Field.getText();
         Random r = new Random();
         int headOrTail = r.nextInt(100) + 1;
-        Pair<Integer, String> winner = null;
-        Pair<Integer, String> loser = null;
         if (headOrTail >= 50) {
-            // head wins
-            if (str1.equals("Heads")){
-                winner = new Pair<>(1, "p1");
-                loser = new Pair<>(2, "p2");
-            }
-            else {
-                winner = new Pair<>(1, "p2");
-                loser = new Pair<>(2, "p1");
-            }
+
         }
         else{
-            if (str1.equals("Tails")){
-                winner = new Pair<>(1, "p2");
-                loser = new Pair<>(2, "p1");
-            }
-            else {
-                winner = new Pair<>(1, "p1");
-                loser = new Pair<>(2, "p2");
-            }
+
         }
-        changeToGameScene(winner, loser, event);
     }
 
     @FXML
     void swap(MouseEvent event) throws IOException{
-        // swaps the head and tail between two players
-        String str1 = player1Field.getText();
-        String str2 = player2Field.getText();
-        player1Field.setText(str2);
-        player2Field.setText(str1);
-    }
-
-    void changeToGameScene(Pair<Integer, String> winner, Pair<Integer, String> loser, MouseEvent event) throws IOException {
-        // saves the data in a data static object and changes scene
-        Data data = new Data(winner, loser, "Player");
-        System.out.println(data);
-        AnchorPane boardScene = FXMLLoader.load(Application.class.getResource("board.fxml"));
-        tokenScene.getChildren().removeAll();
-        tokenScene.getChildren().setAll(boardScene);
+        // how to swap scene
+        Text textField1 = (Text) scene.lookup("p1");
+        Text textField2 = (Text) scene.lookup("p2");
+        String str1 = textField1.getText();
+        String str2 = textField2.getText();
+        textField1.setText(str2);
+        textField2.setText(str1);
     }
 }
