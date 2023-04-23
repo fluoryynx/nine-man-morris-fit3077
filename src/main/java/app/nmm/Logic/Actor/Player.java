8afc29212008f6a69b4881eadcda56a730a38202
gsoo0005 <gsoo0005@monash.edu.my>
2023-04-23 17:31:07 +0800

    Add name attribute to actor class

--- src/main/java/app/nmm/Logic/Actor/Player.java
+++ src/main/java/app/nmm/Logic/Actor/Player.java
@@ -1,6 +1,32 @@
 package app.nmm.Logic.Actor;
 
-import app.nmm.Logic.Actor.Actor;
+import app.nmm.Action.Action;
 
-public class Player extends Actor {
+import java.util.List;
+import java.util.Map;
+
+public class Player extends Actor{
+
+
+    public Player(String tokenColour, String playerName) {
+        super(tokenColour, playerName);
+    }
+
+    @Override
+    public Action playTurn(Map<Integer,List<Action>> allowableActions) {
+        int selectedID =  1;
+        Action finalAction = null;
+        while (finalAction == null){
+            if(allowableActions.containsKey(selectedID)){
+                List<Action> actionList =  allowableActions.get(selectedID);
+                // To be continued........
+            }
+            else{
+                // Get the info and ask the user to choose again
+            }
+        }
+
+
+        return null;
+    }
 }
