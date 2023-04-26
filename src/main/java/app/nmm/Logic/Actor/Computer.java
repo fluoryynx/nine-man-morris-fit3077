0f28eb52271ec4f0497781354bd02ca60084bb79
jchu0057 <jchu0057@student.monash.edu>
2023-04-26 16:46:45 +0800

    created computer class

---
+++ src/main/java/app/nmm/Logic/Actor/Computer.java
@@ -0,0 +1,13 @@
+package app.nmm.Logic.Actor;
+
+import app.nmm.Controller.GameController;
+import app.nmm.Logic.Action.Action;
+
+import java.util.List;
+import java.util.Map;
+
+public class Computer extends Actor {
+    public Computer(String tokenColour, String playerName, int i) {
+        super(tokenColour, playerName, i);
+    }
+}
