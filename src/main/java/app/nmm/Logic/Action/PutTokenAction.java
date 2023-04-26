00f546fed23a95dd98967adeff72baf309f8d5e2
Justin_Chuah <jchu0056@student.monash.edu>
2023-04-26 20:07:36 +0800

    Documentation of Action classes

--- src/main/java/app/nmm/Logic/Action/PutTokenAction.java
+++ src/main/java/app/nmm/Logic/Action/PutTokenAction.java
@@ -10,12 +10,20 @@ import app.nmm.Logic.Actor.Actor;
 import app.nmm.Logic.Location.Node;
 
 public class PutTokenAction extends Action{
-
+    /**
+     *
+     * @param nodeId NodeID of where the token will be placed
+     */
     public PutTokenAction(int nodeId) {
         super(nodeId);
     }
 
-
+    /**
+     * This method creates an instance of the current player's token onto the specified node
+     * Method is only available for the first 9 tokens placed by each player.
+     * @param currentPlayer The player currently taking the turn
+     * @param nodeList List of nodes
+     */
     @Override
     public void execute(Actor currentPlayer, List<Node> nodeList) {
         currentPlayer.subtractTokenInHand();
