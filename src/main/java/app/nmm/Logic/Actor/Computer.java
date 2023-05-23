ac699af275876502028620673fc99474a1c53304
gsoo0005 <gsoo0005@student.monash.edu>
2023-05-24 02:01:45 +0800

    initialise method to get move action

--- src/main/java/app/nmm/Logic/Actor/Computer.java
+++ src/main/java/app/nmm/Logic/Actor/Computer.java
@@ -2,7 +2,12 @@ package app.nmm.Logic.Actor;
 
 import app.nmm.Controller.GameController;
 import app.nmm.Logic.Action.Action;
+import app.nmm.Logic.Action.MoveTokenAction;
+import app.nmm.Logic.Handler.CheckMill;
+import app.nmm.Logic.Location.Node;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -11,7 +16,27 @@ public class Computer extends Actor {
         super(tokenColour, playerName, i);
     }
 
-    public void selectRandomMove(){
-        // TODO: implement select random action
+
+    public Action getMoveAction(Map<Integer,ArrayList<Action>> allowableActionList, ArrayList<Node> nodeList, CheckMill millChecker){
+        Iterator<Integer> tokenPositionList =  allowableActionList.keySet().iterator();
+
+        while(tokenPositionList.hasNext()){
+            int tokenID =  tokenPositionList.next();
+
+            for(Action action : allowableActionList.get(tokenID)){
+
+                int targetID =  ((MoveTokenAction) action).getTargetId();
+
+                ArrayList<Boolean> result = millChecker.checkPossibleMill(nodeList,targetID);
+
+                if (result.get(0) == true || result.get(1) ==  true){
+                    return action;
+                }
+            }
+
+        }
+
+
+        return null;
     }
 }
